bfd4f28d59c6b552ed96b2b60f56cdac20d31ca8
Unknown <jeftasbrotherhood@gmail.com>
2018-07-05 08:36:28 +0200

    Imported Animation

---
+++ Assets/Game/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    private Animator m_Animator;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (m_Animator.GetInteger("State") == 0)
+            {
+                SetAnimationState(1);
+            }
+            else
+            {
+                SetAnimationState(2);
+            }
+        }
+            
+        
+    }
+
+    public void SetAnimationState(int state)
+    {
+        m_Animator.SetInteger("State", state);
+    }
+}
